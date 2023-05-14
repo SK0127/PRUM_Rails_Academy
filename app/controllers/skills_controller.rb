9f4e8e96701b84c893fc7160ed064728d0816ba2
Rikawa-Kairi <pckairi0127@gmail.com>
2023-05-14 15:14:24 +0900

    スキル追加ページ実装

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,8 @@
 class SkillsController < ApplicationController
   def edit
   end
+
+  def new
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-
   get 'profiles/new', to: 'profiles#new'
   post 'profiles', to: 'profiles#create'
-  
   get 'skills/edit', to: 'skills#edit'
+  get 'skills/new', to: 'skills#new'
 end
