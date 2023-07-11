8567df760b825591a1fb65943557cd67eb26f7ca
Rikawa-Kairi <pckairi0127@gmail.com>
2023-07-11 18:48:29 +0900

    topController修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,12 @@
 class TopController < ApplicationController
 
-def index
-  @profile = Profile.where.not(text: '').order(created_at: :desc).first
-  @image = Profile.where.not(image: nil).order(created_at: :desc).first
-  @skill_1 = Skill.where(skill_type: "1")
-  @skill_2 = Skill.where(skill_type: "2")
-  @skill_3 = Skill.where(skill_type: "3")
+  def index
+    @profile = Profile.where.not(text: '').order(created_at: :desc).first
+    @image = Profile.where.not(image: nil).order(created_at: :desc).first
+    @skill_1 = Skill.where(skill_type: "1")
+    @skill_2 = Skill.where(skill_type: "2")
+    @skill_3 = Skill.where(skill_type: "3")
 
-end
+  end
 
 end
