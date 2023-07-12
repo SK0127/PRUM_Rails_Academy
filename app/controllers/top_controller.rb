aab5b84113197c57a93e7161e838813813a70b6a
Rikawa-Kairi <pckairi0127@gmail.com>
2023-07-12 19:18:07 +0900

    debug_test_2

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,10 @@
 class TopController < ApplicationController
 
   def index
-    # @profile = Profile.where.not(text: '').order(created_at: :desc).first
     # <%= @profile.text.truncate(80, omission: '...') %>
-    @image = Profile.where.not(image: nil).order(created_at: :desc).first
+    # @profile = Profile.where.not(text: '').order(created_at: :desc).first
+    # @image = Profile.where.not(image: nil).order(created_at: :desc).first
+    # <%= image_tag @image.image.thumb.url,id: "top_img"%>
     @skill_1 = Skill.where(skill_type: "1")
     @skill_2 = Skill.where(skill_type: "2")
     @skill_3 = Skill.where(skill_type: "3")
